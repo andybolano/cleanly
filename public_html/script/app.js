5bab829dfc5c43771c203a1d2b7553bc78441b1b
User <User@DESKTOP-22OLGMQ>
2017-08-31 20:15:38 -0500

    esqueleto

--- public_html/script/app.js
+++ public_html/script/app.js
@@ -1,39 +1,30 @@
-
- 
-      (function(){
-        'use strict';
-        angular.module('app', ['ui.router'])
-       .constant('HOME', 'app.home')
-     // .constant('API_URL', 'https://birriassoccer.com/equilatero/api/public/api')
-      // .constant('API_URL', 'http://localhost/equilatero/api/public/index.php/api')
-       .config(function($stateProvider, $urlRouterProvider,$compileProvider,$locationProvider) {
-
-          $compileProvider.debugInfoEnabled(false);
-            $urlRouterProvider.otherwise('/');
-
-            $stateProvider
-            //rutas privadas
-                .state('app', {
-                    url: '',
-                    abstract: true,
-                    templateUrl: 'view/template.html'
-                })
-                .state('app.home', {
-                    url: '/',
-                    templateUrl: 'view/home.html',
-                   // controller: 'HomeController as vm'
-                });
-                
+(function () {
+    'use strict';
+    angular.module('app', ['ui.router'])
+            .constant('HOME', 'app.home')
+            // .constant('API_URL', 'https://birriassoccer.com/equilatero/api/public/api')
+            .constant('API_URL', 'http://localhost/cleanly/api/public/index.php/api')
+            .config(function ($stateProvider, $urlRouterProvider, $compileProvider, $locationProvider) {
+                $compileProvider.debugInfoEnabled(false);
+                $urlRouterProvider.otherwise('/');
+                $stateProvider
+                        .state('app', {
+                            url: '',
+                            abstract: true,
+                            templateUrl: 'view/template.html'
+                        })
+                        .state('app.home', {
+                            url: '/',
+                            templateUrl: 'view/home.html',
+                            // controller: 'HomeController as vm'
+                        });
                 $locationProvider.html5Mode(true);
-        })
-        
-   
-
+            });
 })();
 
 angular.module('app').filter('trusted', ['$sce', function ($sce) {
-            return function(url) {
-                return $sce.trustAsResourceUrl(url);
-            };
-        }]);
+        return function (url) {
+            return $sce.trustAsResourceUrl(url);
+        };
+    }]);
 
